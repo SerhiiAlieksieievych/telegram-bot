9d4021ade4531ba0d122dd28101eb6c6fa775335
serhii_alieksieievych <serhiialieksieievych@gmail.com>
2025-02-21 19:05:37 +0200

    Bug with setting initial currency was fixed
    Signed-off-by: serhii_alieksieievych <serhiialieksieievych@gmail.com>

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import codecs
 import time
 from telebot import types
 
-token = "token"
+token = None
 bot = telebot.TeleBot(token)
 
 class CurrencyData:
@@ -272,7 +272,6 @@ def start(message):
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback(call):
-    bot_handler.set_initial_currency(bot_handler.initial_currency_name)
     message, data = call.message, call.data
     bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
     if data == 'restart':
@@ -291,6 +290,7 @@ def callback(call):
         bot.send_message(message.chat.id, "Зробити ще одну конвертацію?", reply_markup=markup)
         return
     if data != 'else':
+        bot_handler.set_initial_currency(bot_handler.initial_currency_name)
         bot.send_message(message.chat.id, f"{data}")
         bot_handler.set_target_currency(data)
         conversion_request = RequestToMonobank()
